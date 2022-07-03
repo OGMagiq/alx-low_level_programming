40ef0eadd51a6a70f59e5a5f290314613c4718a5
OGMagiq <ogmagic1997@gmail.com>
2022-07-03 15:18:06 -0700

    task puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
- * Return: 0 Always (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
+
 	return (0);
 }
