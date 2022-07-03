8a16378eaf830c5fedfb5b4be547c7350291c7ac
OGMagiq <ogmagic1997@gmail.com>
2022-07-03 14:39:50 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- * how to print using puts
  *
  * main - Entry point
  *
@@ -10,6 +9,6 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
