4b3f7d0834d12186002f71b8ecab9a19e7998825
OGMagiq <ogmagic1997@gmail.com>
2022-07-04 03:58:07 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s/n", n);
+
+	return (1);
+}
